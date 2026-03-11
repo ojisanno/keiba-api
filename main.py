4e9cc3d85b59f287540a334efa6d57063c8f3f68
ojisanno <24kansen@gmail.com>
2026-03-11 21:09:17 +0900

    fix conflict in main.py

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/hello")
 def hello():
-=======
-from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get("/hello")
-def hello():
->>>>>>> fe04360e4533af6d70982d128416b446990ebc21
     return {"message": "クラウドでPythonが動いています"}
+
